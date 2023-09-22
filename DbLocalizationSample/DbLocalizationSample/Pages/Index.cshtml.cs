a5d5d987ef14c46f7f7af76e80f82634b9050005
Ghislain <lerda.ghislain@gmail.com>
2023-09-22 17:33:32 +0200

    Update Index.cshtml.cs

--- DbLocalizationSample/DbLocalizationSample/Pages/Index.cshtml.cs
+++ DbLocalizationSample/DbLocalizationSample/Pages/Index.cshtml.cs
@@ -1,40 +1,110 @@
-﻿using System;
+using DbLocalizationSample;
+using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
-namespace DBLocalizationSample.Pages
+namespace DBLocalizationSample.Pages;
+
+public class FormCreateCommand
 {
-    public class IndexModel : PageModel
+    [Display(Name = "Firstname")]
+    public string Firstname { get; set; }
+
+    [Display(Name = "Lastname")]
+    public string Lastname { get; set; }
+
+    [Display(Name = "Phone number")]
+    public string PhoneNumber { get; set; }
+
+    [Display(Name = "Email address")]
+    public string Email { get; set; }
+
+    [Display(Name = "Nationality")]
+    public string Nationality { get; set; }
+
+    [Display(Name = "Profession")]
+    public string Profession { get; set; }
+
+    [Display(Name = "Country of Residence")]
+    public string CountryOfResidence { get; set; }
+
+    [Display(Name = "Address")]
+    public string Address { get; set; }
+
+    [Display(Name = "Are you considering taking up residence in the Principality?")]
+    public string Residency { get; set; }
+
+    [Display(Name = "Spoken language(s)")]
+    public List<string> Languages { get; set; } = new List<string>();
+
+    [Display(Name = "How do you know CFM Indosuez Wealth Management?")]
+    public List<string> KnowCfm { get; set; } = new List<string>();
+
+    [Display(Name = "What are your expectations ?")]
+    public List<string> Expectations { get; set; } = new List<string>();
+
+    [Display(Name = "What are your needs ?")]
+    public List<string> Needs { get; set; } = new List<string>();
+}
+
+public class FormCreateCommandValidator : AbstractValidator<FormCreateCommand>
+{
+    public FormCreateCommandValidator(IStringLocalizer localizer)
     {
-        private readonly ILogger<IndexModel> _logger;
+        RuleFor(x => x.Firstname).NotEmpty().Length(5, 30).WithName(x => localizer[nameof(x.Firstname)]);
+        RuleFor(x => x.Lastname).NotEmpty().Length(5, 30).WithName(x => localizer[nameof(x.Lastname)]);
+        RuleFor(x => x.PhoneNumber).NotEmpty().Length(5, 30).WithName(x => localizer[nameof(x.PhoneNumber)]);
+        RuleFor(x => x.Email).NotEmpty().EmailAddress().WithName(x => localizer[nameof(x.Email)]);
+        RuleFor(x => x.Nationality).NotEmpty().WithName(x => localizer[nameof(x.Nationality)]);
+        RuleFor(x => x.Profession).NotEmpty().Length(5, 30).WithName(x => localizer[nameof(x.Profession)]);
+        RuleFor(x => x.CountryOfResidence).NotEmpty().WithName(x => localizer[nameof(x.CountryOfResidence)]);
+        RuleFor(x => x.Address).NotEmpty().Length(5, 30).WithName(x => localizer[nameof(x.Address)]);
+        RuleFor(x => x.Residency).NotEmpty().WithName(x => localizer[nameof(x.Residency)]);
+        RuleFor(x => x.Languages).Must(x => x.Count > 0).WithName(x => localizer[nameof(x.Languages)]);
+        RuleFor(x => x.KnowCfm).Must(x => x.Count > 0).WithName(x => localizer[nameof(x.KnowCfm)]);
+        RuleFor(x => x.Expectations).Must(x => x.Count > 0).WithName(x => localizer[nameof(x.Expectations)]);
+        RuleFor(x => x.Needs).Must(x => x.Count > 0).WithName(x => localizer[nameof(x.Needs)]);
+    }
+}
 
-        public IndexModel(ILogger<IndexModel> logger)
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public FormCreateCommand Command { get; set; }
+
+    public void OnGet()
+    {
+
+    }
+
+    public async Task<IActionResult> OnPost([FromServices] IValidator<FormCreateCommand> validator)
+    {
+        var result = await validator.ValidateAsync(Command);
+
+        if (!result.IsValid)
         {
-            _logger = logger;
+            result.AddToModelState(ModelState);
+            return Page();
         }
 
-        public void OnGet()
-        {
+        return RedirectToPage("/Index");
+    }
 
-        }
+    public IActionResult OnGetSetCultureCookie(string cltr, string returnUrl)
+    {
+        Response.Cookies.Append(
+            CookieRequestCultureProvider.DefaultCookieName,
+            CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(cltr)),
+            new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+        );
 
-        /// <summary>
-        /// Set culture cookie value on culture change
-        /// </summary>
-        /// <param name="cltr">Culture name</param>
-        /// <param name="returnUrl">The url to return to after setting the cookie</param>
-        public IActionResult OnGetSetCultureCookie(string cltr, string returnUrl)
-        {
-            Response.Cookies.Append(
-                CookieRequestCultureProvider.DefaultCookieName,
-                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(cltr)),
-                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
-            );
-
-            return LocalRedirect(returnUrl);
-        }
+        return LocalRedirect(returnUrl);
     }
 }
